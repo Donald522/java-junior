cf5b2825443d7d95edf99ffb05b5509c81ee20bc
Dmitry Bulanov <bulanovdg@gmail.com>
2016-08-23 17:17:18 +0300

    Concatinate methods to String and Object + ?:

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,23 +1,11 @@
 package com.acme.edu;
 
 public class Logger {
-    public static void log(int message) {
-        System.out.println("primitive: " + message);
-    }
+    public static void log(int message) { System.out.println("primitive: " + message); }
 
-    public static void log(char message) {
-        System.out.println("char: " + message);
-    }
+    public static void log(char message) { System.out.println("char: " + message); }
 
-    public static void log(String message) {
-        System.out.println("string: " + message);
-    }
+    public static void log(boolean message) { System.out.println("primitive: " + message); }
 
-    public static void log(boolean message) {
-        System.out.println("primitive: " + message);
-    }
-
-    public static void log(Object message) {
-        System.out.println("reference: " + message);
-    }
+    public static void log(Object message) { System.out.println((message instanceof String) ? "string: " + message : "reference: " + message); }
 }
