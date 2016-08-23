e2407c8d8c1f023f4c30fba4839e53f476fccb3c
Anton Tolkachev <anton-t@inbox.ru>
2016-08-23 17:51:03 +0300

    Universal method for loggind was added

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,11 +1,18 @@
 package com.acme.edu;
 
 public class Logger {
-    public static void log(int message) { System.out.println("primitive: " + message); }
+    public static void log(int message) { logMessagePrinter("primitive: " + message); }
 
-    public static void log(char message) { System.out.println("char: " + message); }
+    public static void log(char message) { logMessagePrinter("char: " + message); }
 
-    public static void log(boolean message) { System.out.println("primitive: " + message); }
+    public static void log(boolean message) { logMessagePrinter("primitive: " + message); }
 
-    public static void log(Object message) { System.out.println((message instanceof String) ? "string: " + message : "reference: " + message); }
+    public static void log(Object message) {
+        logMessagePrinter((message instanceof String) ?
+                "string: " + message : "reference: " + message);
+    }
+
+    private static void logMessagePrinter(String message) {
+        System.out.println(message);
+    }
 }
