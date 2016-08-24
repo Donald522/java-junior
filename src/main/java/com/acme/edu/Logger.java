93203e4747488eff079f7cab2f99856daa0621a8
Anton Tolkachev <anton-t@inbox.ru>
2016-08-24 12:01:57 +0300

    Refactor methods

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,11 +1,18 @@
 package com.acme.edu;
 
 public class Logger {
-    public static void log(int message) { logMessagePrinter("primitive: " + message); }
 
-    public static void log(char message) { logMessagePrinter("char: " + message); }
+    public static void log(int message) {
+        logMessagePrinter("primitive: " + message);
+    }
 
-    public static void log(boolean message) { logMessagePrinter("primitive: " + message); }
+    public static void log(char message) {
+        logMessagePrinter("char: " + message);
+    }
+
+    public static void log(boolean message) {
+        logMessagePrinter("primitive: " + message);
+    }
 
     public static void log(Object message) {
         logMessagePrinter((message instanceof String) ?
