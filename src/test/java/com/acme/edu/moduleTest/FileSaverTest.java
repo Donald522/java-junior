c381fbdab6f1ba371012811c73d0d58e6ad67a8f
Dmitry Bulanov <bulanovdg@gmail.com>
2016-08-31 11:28:08 +0300

    Deleted oldTest

--- src/test/java/com/acme/edu/moduleTest/FileSaverTest.java
+++ src/test/java/com/acme/edu/moduleTest/FileSaverTest.java
@@ -24,12 +24,7 @@ public class FileSaverTest {
     //region given
     @Before
     public void setUpSystemOut() throws IOException, LoggerException {
-//        fileSaver = new FileSaver();
-//        if (((FileSaver)fileSaver).getLogFile().exists()) {
-//            ((FileSaver) fileSaver).getLogFile().delete();
-//        }
-//        fileSaver = new FileSaver();
-//        loggerFacade = new LoggerFacade(fileSaver, new ConsoleSaver());
+
     }
 
     @After
@@ -47,32 +42,6 @@ public class FileSaverTest {
 //        loggerFacade.log(-2);
 //        loggerFacade.flush();
 //    }
-
-    @Test
-    public void shouldWriteMessagesToFile() throws LoggerException {
-        File logfile = ((FileSaver)fileSaver).getLogFile();
-
-        fileSaver.save("cool message1" + System.lineSeparator());
-        fileSaver.save("cool message2" + System.lineSeparator());
-
-        String readedLine = "";
-
-        try (BufferedReader bufferedReader = new BufferedReader(
-                new InputStreamReader(
-                        new BufferedInputStream(
-                                new FileInputStream(logfile)), "UTF-8"))) {
-
-
-            for (int i = 0; i < 2; i++) {
-                readedLine += bufferedReader.readLine() + System.lineSeparator();
-            }
-        } catch (IOException e) {
-            throw new AppendException("Can't read message from log file: ", e);
-        }
-        assertEquals("cool message1" + System.lineSeparator() +
-                "cool message2" + System.lineSeparator(),
-                readedLine);
-    }
 
     @Test
     public void shouldCreateFileByString() throws AppendException{
